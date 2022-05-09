bb068edd3b879ae321147f78abd9ffa2c941c517
edenleluan <105213432+edenleluan@users.noreply.github.com>
2022-05-09 15:44:47 +0100

    push

---
+++ FinalProject/java-ui-dashboard-008-main/ui-dashboard-008/src/com/raven/main/Main.java
@@ -0,0 +1,153 @@
+package com.raven.main;
+
+import com.raven.event.EventMenu;
+import com.raven.form.Form1;
+import com.raven.form.SubForm;
+import com.raven.swing.scrollbar.ScrollBarCustom;
+import java.awt.Color;
+import java.awt.Component;
+
+public class Main extends javax.swing.JFrame {
+
+    public Main() {
+        initComponents();
+        setBackground(new Color(0, 0, 0, 0));
+        EventMenu eventMenu = new EventMenu() {
+            @Override
+            public void selected(int index) {
+                if (index == 0) {
+                    showForm(new Form1());
+                } else {
+                    showForm(new SubForm(index));
+                }
+            }
+        };
+        EventMenu eventHeader = new EventMenu() {
+            @Override
+            public void selected(int index) {
+                System.out.println("Header Index " + index);
+            }
+        };
+        header.init(eventHeader);
+        menu.initWinButton(Main.this, panelBackground1);
+        menu.initMenu(eventMenu);
+        scroll.setVerticalScrollBar(new ScrollBarCustom());
+        scroll.getViewport().setOpaque(false);
+        scroll.setViewportBorder(null);
+        showForm(new Form1());
+    }
+
+    private void showForm(Component com) {
+        body.removeAll();
+        body.add(com);
+        body.revalidate();
+        body.repaint();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        panelBackground1 = new com.raven.swing.PanelBackground();
+        menu = new com.raven.component.Menu();
+        scroll = new javax.swing.JScrollPane();
+        body = new javax.swing.JPanel();
+        header = new com.raven.component.Header();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setUndecorated(true);
+
+        panelBackground1.setForeground(new java.awt.Color(102, 204, 255));
+
+        scroll.setBorder(null);
+        scroll.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+        body.setBackground(new java.awt.Color(255, 255, 255));
+        body.setOpaque(false);
+        body.setLayout(new java.awt.BorderLayout());
+
+        header.setBackground(new java.awt.Color(102, 204, 255));
+        body.add(header, java.awt.BorderLayout.CENTER);
+
+        scroll.setViewportView(body);
+
+        javax.swing.GroupLayout panelBackground1Layout = new javax.swing.GroupLayout(panelBackground1);
+        panelBackground1.setLayout(panelBackground1Layout);
+        panelBackground1Layout.setHorizontalGroup(
+            panelBackground1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelBackground1Layout.createSequentialGroup()
+                .addComponent(menu, javax.swing.GroupLayout.PREFERRED_SIZE, 219, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(44, 44, 44)
+                .addComponent(scroll, javax.swing.GroupLayout.DEFAULT_SIZE, 999, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        panelBackground1Layout.setVerticalGroup(
+            panelBackground1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelBackground1Layout.createSequentialGroup()
+                .addGap(52, 52, 52)
+                .addComponent(scroll, javax.swing.GroupLayout.DEFAULT_SIZE, 677, Short.MAX_VALUE)
+                .addContainerGap())
+            .addGroup(panelBackground1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(menu, javax.swing.GroupLayout.PREFERRED_SIZE, 714, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(panelBackground1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(panelBackground1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Main().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel body;
+    private com.raven.component.Header header;
+    private com.raven.component.Menu menu;
+    private com.raven.swing.PanelBackground panelBackground1;
+    private javax.swing.JScrollPane scroll;
+    // End of variables declaration//GEN-END:variables
+}
